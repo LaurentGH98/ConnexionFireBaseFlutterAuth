1272aa0db40fe4061bb819ee2265bff3363ebacb
Laurent Ghiassi <laurent.ghiassi@gmail.com>
2023-04-12 23:40:30 +0200

    first commit

---
+++ lib/widget_tree.dart
@@ -0,0 +1,28 @@
+import '../auth.dart';
+import 'package:premier_essai_auth_firebase/pages/home_page.dart';
+import 'package:premier_essai_auth_firebase/pages/login_register_page.dart';
+import 'package:flutter/material.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth().authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.hasData){
+          return HomePage();
+        }
+        else{
+          return const LoginPage();
+        }
+      });
+  }
+}
+
